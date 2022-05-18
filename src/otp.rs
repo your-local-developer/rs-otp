05c0db5df7b0c1f973e8c26030a222925ed0d303
Jan <your-local-developer@git.slmail.me>
2022-05-18 19:57:49 +0200

    Add better function comments for Otp trait

--- src/otp.rs
+++ src/otp.rs
@@ -2,11 +2,17 @@ use crate::algorithm::Algorithm;
 use data_encoding::{DecodeError, BASE32, BASE32_NOPAD};
 use std::io::Error;
 
-pub(crate) trait Otp {
+pub trait Otp {
     /// Returns an instance taking the unencoded secret as vector of bytes.
+    /// The default algorithm is SHA-1.
+    /// To be RFC compliant, the number od digits should be between 6 and 10.
+    /// Numbers higher than 10 will receive leading zeros.
     fn new(secret: Vec<u8>, algorithm: Algorithm, digits: u8) -> Self;
 
     /// Returns an instance taking the Base32 encoded secret as string.
+    /// The default algorithm is SHA-1.
+    /// To be RFC compliant, the number od digits should be between 6 and 10.
+    /// Numbers higher than 10 will receive leading zeros.
     fn from_base32_string(
         secret: &str,
         algorithm: Algorithm,
@@ -16,16 +22,20 @@ pub(crate) trait Otp {
         Self: Sized;
 
     /// Returns an instance taking the unencoded secret as string.
+    /// The default algorithm is SHA-1.
+    /// To be RFC compliant, the number od digits should be between 6 and 10.
+    /// Numbers higher than 10 will receive leading zeros.
     fn from_string(secret: &str, algorithm: Algorithm, digits: u8) -> Self;
 
     /// Validates a Base32 encoded secret while ignoring any spaces.
-    fn validate(secret: &str) -> bool {
+    fn validate_secret(secret: &str) -> bool {
         Self::decode_secret(secret).is_ok()
     }
 
     /// Validates a Base32 encoded secret including its unencoded length while ignoring any spaces.
     /// Returns `false` if the length is smaller than 128 Bit.
-    fn validate_len(secret: &str) -> bool {
+    /// This is not included in `validate_secret` function, because Google Authenticator used to produce secrets, which did not confirm to the RFC.`
+    fn validate_secret_len(secret: &str) -> bool {
         if let Ok(decoded_secret) = Self::decode_secret(secret) {
             decoded_secret.len() >= 16
         } else {
@@ -34,6 +44,7 @@ pub(crate) trait Otp {
     }
 
     /// Decodes a Base32 encoded secret while removing any spaces.
+    /// It accepts padded and non-padded secrets.
     fn decode_secret(secret: &str) -> Result<Vec<u8>, DecodeError> {
         let secret = secret.replace(' ', "").to_uppercase();
         if secret.contains('=') {
@@ -43,9 +54,13 @@ pub(crate) trait Otp {
         }
     }
 
-    /// Calculates the Otp value taking the given counter.
+    /// Calculates the u32 Otp code taking a counter or time based value as moving factor.
+    /// It uses dynamic truncation to calculate an offset.
+    /// This is the preferred method.
     fn calculate(&self, counter: u64) -> Result<u32, Error>;
 
-    /// Calculates the Otp value taking the given counter and offset.
+    /// Calculates the u32 Otp code taking a counter or time based value as moving factor.
+    /// It uses a custom offset to extract 4 bytes from the HMAC-SHA Digest.
+    /// Keep in mind that the max value of the offset is the last index of the resulting digest minus four bytes.
     fn calculate_with_offset(&self, counter: u64, offset: u8) -> Result<u32, Error>;
 }
